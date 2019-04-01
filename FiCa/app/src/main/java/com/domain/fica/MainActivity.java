e9be73181e1ee332afafd26fc1937fc62e613270
Coen <coenrijsdijk@gmail.com>
2019-04-01 17:42:53 +0200

    more fixes on nav bar

--- FiCa/app/src/main/java/com/domain/fica/MainActivity.java
+++ FiCa/app/src/main/java/com/domain/fica/MainActivity.java
@@ -1,5 +1,6 @@
 package com.domain.fica;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -102,9 +103,11 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_movie_overview) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_list_overview) {
+            Intent intent = new Intent(MainActivity.this, ListOverviewActivity.class);
+            startActivity(intent);
 
         }
 
