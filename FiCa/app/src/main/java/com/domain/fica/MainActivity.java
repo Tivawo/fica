876f817dfddc7e171d6a09d97ccb251749ccdfcc
RobinBeljaars <rp.beljaars@student.avans.nl>
2019-04-01 12:42:01 +0200

    Menu build for the MainActivity
    the buttons for share, print, sort, filter and filmoverview are added

--- FiCa/app/src/main/java/com/domain/fica/MainActivity.java
+++ FiCa/app/src/main/java/com/domain/fica/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recycler;
@@ -18,5 +21,21 @@ public class MainActivity extends AppCompatActivity {
         recycler.setLayoutManager(layoutManager);
         recycler.setHasFixedSize(true);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.share) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
