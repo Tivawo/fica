038601c101f46101cc7e1646505537e9bb789e94
RobinBeljaars <rp.beljaars@student.avans.nl>
2019-04-01 19:22:28 +0200

    Update ListOverviewActivity.java
    navigatie bar van main (Sort, print share etc) verwijderd sinds dat er niet in thuishoort

--- FiCa/app/src/main/java/com/domain/fica/ListOverviewActivity.java
+++ FiCa/app/src/main/java/com/domain/fica/ListOverviewActivity.java
@@ -44,31 +44,6 @@ public class ListOverviewActivity extends AppCompatActivity implements Navigatio
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.share) {
-            return true;
-        }  else if (id == R.id.Filter_Genre){
-            startActivity(new Intent(ListOverviewActivity.this, GenreFilter.class));
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
