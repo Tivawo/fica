6ca2d93b82c2a52970c1d6a5486e4ab85f81f0fb
RobinBeljaars <rp.beljaars@student.avans.nl>
2019-04-02 15:44:47 +0200

    Print button in details implemented

--- FiCa/app/src/main/java/com/domain/fica/DetailActivity.java
+++ FiCa/app/src/main/java/com/domain/fica/DetailActivity.java
@@ -1,35 +1,25 @@
 package com.domain.fica;
 
-import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.pdf.PdfDocument;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.print.PrintManager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.WindowManager;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.domain.fica.Domain.Movie;
+import com.domain.fica.Utils.DetailPrintDocumentAdapter;
 import com.squareup.picasso.Picasso;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 
 import Data.Genres;
 
@@ -58,12 +48,11 @@ public class DetailActivity extends AppCompatActivity {
         tvMovieRating = findViewById(R.id.tv_movie_rating);
         tvMovieDescription = findViewById(R.id.tv_movie_description);
 
-
         Bundle extras = getIntent().getExtras();
 
-        Movie movie = (Movie) extras.getSerializable("MOVIE");
+        Movie movie = (Movie)extras.getSerializable("MOVIE");
 
-        if (movie.isAdult()) {
+        if(movie.isAdult()){
             age = "Adult";
         } else {
             age = "Child/teen";
@@ -92,7 +81,6 @@ public class DetailActivity extends AppCompatActivity {
         MenuItem menuItem = menu.findItem(R.id.share);
         menuItem.setIntent(shareMovie());
         return true;
-        //force the commit
     }
 
     @Override
@@ -101,10 +89,10 @@ public class DetailActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.print) {
-            bitmap = loadBitmapFromView(Testprint, Testprint.getWidth(), Testprint.getHeight());
-            createPdf();
             String dir = Environment.getExternalStorageDirectory().getAbsolutePath();
-            Log.d("DetailActivity a", dir);
+            bitmap = loadBitmapFromView(Testprint, Testprint.getWidth(), Testprint.getHeight());
+            printDocument(Testprint);
+            Log.d("DetailActivity a",dir);
             return true;
         } else if (id == R.id.share) {
 
@@ -112,70 +100,6 @@ public class DetailActivity extends AppCompatActivity {
             finish();
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    private void createPdf() {
-        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
-        //  Display display = wm.getDefaultDisplay();
-        DisplayMetrics displaymetrics = new DisplayMetrics();
-        this.getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
-        float hight = displaymetrics.heightPixels;
-        float width = displaymetrics.widthPixels;
-
-        int convertHighet = (int) hight, convertWidth = (int) width;
-
-//        Resources mResources = getResources();
-//        Bitmap bitmap = BitmapFactory.decodeResource(mResources, R.drawable.screenshot);
-
-        PdfDocument document = new PdfDocument();
-        PdfDocument.PageInfo pageInfo = new PdfDocument.PageInfo.Builder(convertWidth, convertHighet, 1).create();
-        PdfDocument.Page page = document.startPage(pageInfo);
-
-        Canvas canvas = page.getCanvas();
-
-        Paint paint = new Paint();
-        canvas.drawPaint(paint);
-
-        bitmap = Bitmap.createScaledBitmap(bitmap, convertWidth, convertHighet, true);
-
-        paint.setColor(Color.BLUE);
-        canvas.drawBitmap(bitmap, 0, 0, null);
-        document.finishPage(page);
-
-        // write the document content
-        String targetPdf = "/sdcard/pdffromlayout.pdf";
-        File filePath;
-        filePath = new File(targetPdf);
-        try {
-            document.writeTo(new FileOutputStream(filePath));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            Toast.makeText(this, "Something wrong: " + e.toString(), Toast.LENGTH_LONG).show();
-        }
-
-        // close the document
-        document.close();
-        Toast.makeText(this, "PDF is created!!!", Toast.LENGTH_SHORT).show();
-
-        openGeneratedPDF();
-
-    }
-
-    private void openGeneratedPDF() {
-        File file = new File("/sdcard/pdffromlayout.pdf");
-        if (file.exists()) {
-            Intent intent = new Intent(Intent.ACTION_VIEW);
-            Uri uri = Uri.fromFile(file);
-            intent.setDataAndType(uri, "application/pdf");
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
-            try {
-                startActivity(intent);
-            } catch (ActivityNotFoundException e) {
-                Toast.makeText(DetailActivity.this, "No Application available to view pdf", Toast.LENGTH_LONG).show();
-            }
-        }
     }
 
     public static Bitmap loadBitmapFromView(View v, int width, int height) {
@@ -198,4 +122,15 @@ public class DetailActivity extends AppCompatActivity {
         return sharingIntent;
     }
 
+    public void printDocument(View view)
+    {
+        PrintManager printManager = (PrintManager) this
+                .getSystemService(Context.PRINT_SERVICE);
+
+        String jobName = this.getString(R.string.app_name) +
+                " Document";
+
+        printManager.print(jobName, new DetailPrintDocumentAdapter(this, bitmap),
+                null);
+    }
 }
