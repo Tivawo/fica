0588eb63f1991a24531af336a01d2cade3ae0acd
Tivawo <timvanwouwe@upcmail.nl>
2019-04-03 14:49:19 +0200

    Bug fix
    Image backdrop sometimes did not cover the entire background in detailview

--- FiCa/app/src/main/java/com/domain/fica/Domain/Movie.java
+++ FiCa/app/src/main/java/com/domain/fica/Domain/Movie.java
@@ -87,6 +87,7 @@ public class Movie implements Serializable {
     public double getVoteAvg() {
         return voteAvg;
     }
+
     public ArrayList<String> getReviews() {
         return reviews;
     }
